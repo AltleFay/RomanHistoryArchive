b43078067f986e51520e3f98d221584d18e0cd3c
AltleFay <altlefay@gmail.com>
2026-02-23 19:55:55 +0700

    update

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\EventApiController;
+use App\Http\Controllers\Api\EraApiController;
+
+Route::get('/events', [EventApiController::class, 'index']);
+Route::post('/events', [EventApiController::class, 'store']);
+Route::get('/events/{event}', [EventApiController::class, 'show']);
+Route::put('/events/{event}', [EventApiController::class, 'update']);
+Route::delete('/events/{event}', [EventApiController::class, 'destroy']);
+
+Route::get('/eras', [EraApiController::class, 'index']);
+Route::post('/eras', [EraApiController::class, 'store']);
+Route::get('/eras/{era}', [EraApiController::class, 'show']);
+Route::put('/eras/{era}', [EraApiController::class, 'update']);
+Route::delete('/eras/{era}', [EraApiController::class, 'destroy']);
--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,34 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
-use App\Http\Controllers\Api\EventApiController;
-use App\Http\Controllers\Api\EraApiController;
+use App\Http\Controllers\ProfileController;
 
-Route::get('/timeline', [EventController::class, 'index']);
-
-// API Routes
-Route::prefix('api')->group(function () {
-    // Event API Routes
-    Route::get('/events', [EventApiController::class, 'index']);
-    Route::post('/events', [EventApiController::class, 'store']);
-    Route::get('/events/{event}', [EventApiController::class, 'show']);
-    Route::put('/events/{event}', [EventApiController::class, 'update']);
-    Route::delete('/events/{event}', [EventApiController::class, 'destroy']);
-    
-    // Era API Routes
-    Route::get('/eras', [EraApiController::class, 'index']);
-    Route::post('/eras', [EraApiController::class, 'store']);
-    Route::get('/eras/{era}', [EraApiController::class, 'show']);
-    Route::put('/eras/{era}', [EraApiController::class, 'update']);
-    Route::delete('/eras/{era}', [EraApiController::class, 'destroy']);
+Route::get('/', function () {
+    return view('welcome');
 });
+
+/*
+|--------------------------------------------------------------------------
+| Protected Modules (Login Required)
+|--------------------------------------------------------------------------
+*/
+Route::middleware(['auth', 'verified'])->group(function () {
+
+    Route::get('/timeline', [EventController::class, 'index'])
+        ->name('timeline');
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])
+        ->name('profile.edit');
+
+    Route::patch('/profile', [ProfileController::class, 'update'])
+        ->name('profile.update');
+
+    Route::delete('/profile', [ProfileController::class, 'destroy'])
+        ->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
